9859ce90eeaffac3ba33b027bd6fee9b14fa5e17
samin <samin182020@gmail.com>
2024-01-09 22:06:48 +0600

    Adding UI ..

--- src/com/rupcharcha/Main.java
+++ src/com/rupcharcha/Main.java
@@ -2,6 +2,8 @@ package com.rupcharcha;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Library Management System!");
+        System.out.println("------Library Management System!----------");
+        System.out.println("\n1 for Librarian");
+        System.out.println("2 for User");
     }
 }
