0bea36af0daf78e2356246fd7812481b088f1da4
alamin5g <21303134@iubat.edu>
2024-01-09 22:17:58 +0600

    testing

--- src/com/rupcharcha/Main.java
+++ src/com/rupcharcha/Main.java
@@ -3,5 +3,10 @@ package com.rupcharcha;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Library Management System!");
+
+        System.out.println("This is just test");
+        System.out.println("nothin");
+
+
     }
 }
